4f56b16e58e2dd86f116848e4410cda11f56b655
unknown <suongntn131@gmail.com>
2022-05-06 15:11:57 +0700

    lession-17

---
+++ src/test/java/test/lession_17/HybridContext.java
@@ -0,0 +1,99 @@
+package test.lession_17;
+
+import context.Contexts;
+import context.WaitMoreThanOneContext;
+import driver.DriverFactory;
+import driver.Platforms;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HybridContext {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> driver = DriverFactory.getDriver(Platforms.android);
+        try {
+            // Navigate to webview screen
+            By webviewNavBtnSel = MobileBy.AccessibilityId("Webview");
+            MobileElement webviewNavBtnElem = driver.findElement(webviewNavBtnSel);
+            webviewNavBtnElem.click();
+
+            // Wait until we have more than 1 context
+            WebDriverWait wait = new WebDriverWait(driver, 15L);
+            wait.until(new WaitMoreThanOneContext(driver));
+
+            // Get all context names
+            driver.getContextHandles().forEach(context -> {
+                System.out.println(context);
+            });
+
+            // Switch to webview context
+            driver.context(Contexts.WEB_VIEW);
+
+            // Interact on webview elements
+            WebElement navToggleBtnElem = driver.findElementByCssSelector(".navbar__toggle");
+            navToggleBtnElem.click();
+
+            List<MobileElement> menuItemElems = driver.findElements(By.cssSelector(".menu__list li a"));
+            List<MenuItemData> menuItemDataList = new ArrayList<>();
+            if (menuItemElems.isEmpty()){
+                throw new RuntimeException("[ERR] There is no list item!");
+            }
+
+            for (MobileElement menuItemElem : menuItemElems) {
+                String itemText = menuItemElem.getText();
+                String itemHref = menuItemElem.getAttribute("href");
+                if (itemText.isEmpty())
+                    menuItemDataList.add(new MenuItemData("Github", itemHref));
+                else
+                    menuItemDataList.add(new MenuItemData(itemText,itemHref));
+            }
+
+            for (MenuItemData menuItemData : menuItemDataList) {
+                System.out.println(menuItemData.getName());
+                System.out.println(menuItemData.getHref());
+            }
+
+            // Switch back to NATIVE context
+            driver.context(Contexts.NATIVE);
+            // Click nav login
+            MobileElement navLoginBtnElem = driver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginBtnElem.click();
+
+            // Fill login form
+            MobileElement txtUsernameElem = driver.findElement(MobileBy.AccessibilityId("input-email"));
+            MobileElement txtPasswordElem = driver.findElement(MobileBy.AccessibilityId("input-password"));
+            MobileElement btnLoginElem = driver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
+            txtUsernameElem.sendKeys("test@gmail.com");
+            txtPasswordElem.sendKeys("12345678");
+            btnLoginElem.click();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+
+    public static class MenuItemData{
+        private String name;
+        private String href;
+
+        public MenuItemData(String name, String href) {
+            this.name = name;
+            this.href = href;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getHref() {
+            return href;
+        }
+    }
+}
---
+++ src/test/java/test/lession_17/TakingScreenshot.java
@@ -0,0 +1,43 @@
+package test.lession_17;
+
+import driver.DriverFactory;
+import driver.Platforms;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+
+import java.io.File;
+
+public class TakingScreenshot {
+    public static void main(String[] args) {
+
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platforms.android);
+        try {
+            MobileElement navLoginBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginBtnElem.click();
+            // Whole screen
+            File basic64ScreenshotData = appiumDriver.getScreenshotAs(OutputType.FILE);
+            String fileLocation = System.getProperty("user.dir").concat("/screenshots/").concat("HomeScreen.png");
+            FileUtils.copyFile(basic64ScreenshotData,new File(fileLocation));
+
+            // An area
+            MobileElement loginFormElem = appiumDriver.findElement(MobileBy.AccessibilityId("Login-screen"));
+            File basic64LoginFormData = loginFormElem.getScreenshotAs(OutputType.FILE);
+            String loginFormFileLocation = System.getProperty("user.dir").concat("/screenshots/").concat("LoginForm.png");
+            FileUtils.copyFile(basic64LoginFormData,new File(loginFormFileLocation));
+
+            // An element
+            MobileElement loginBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
+            File basic64LoginBtnData = loginBtnElem.getScreenshotAs(OutputType.FILE);
+            String loginBtnFileLocation = System.getProperty("user.dir").concat("/screenshots/").concat("LoginBtn.png");
+            FileUtils.copyFile(basic64LoginBtnData,new File(loginBtnFileLocation));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
+}
